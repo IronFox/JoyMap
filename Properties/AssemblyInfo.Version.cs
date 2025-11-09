11794a150fcf2f8dc3fd5bb86c7133021c76c741
IronFox <github@portal.delta-works.org>
2025-11-09 11:32:41 +0100

    Disable workflow

--- Properties/AssemblyInfo.Version.cs
+++ Properties/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.Versioning;
-[assembly: AssemblyVersion("1.0.29.0")]
-[assembly: AssemblyFileVersion("1.0.29.0")]
-[assembly: AssemblyInformationalVersion("1.0.29")]
+[assembly: AssemblyVersion("1.0.34.0")]
+[assembly: AssemblyFileVersion("1.0.34.0")]
+[assembly: AssemblyInformationalVersion("1.0.34")]
 [assembly: SupportedOSPlatform("windows")]
