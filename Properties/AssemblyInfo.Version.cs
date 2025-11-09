78d082d81c7dfc0d898145dc076d934ff933d746
IronFox <github@portal.delta-works.org>
2025-11-09 14:52:18 +0100

    Increment build number

--- Properties/AssemblyInfo.Version.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.Versioning;
-[assembly: AssemblyVersion("1.0.37.0")]
-[assembly: AssemblyFileVersion("1.0.37.0")]
-[assembly: AssemblyInformationalVersion("1.0.37")]
-[assembly: SupportedOSPlatform("windows")]
